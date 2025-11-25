f42223667d47465bd0fc39b1aeb7e77458b40a3c
VikramMugale <mugalevikram26@gmail.com>
2025-11-25 13:47:47 +0530

    fix: correct database configuration settings

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://127.0.0.1:27017/testing', {
+    const conn = await mongoose.connect('mongodb+srv://delta:9VYFibSmGJILecf1@cluster0.tle7v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
