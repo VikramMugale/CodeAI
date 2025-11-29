7717f0103501629cbc3d26e54eca19f103453df4
VikramMugale <mugalevikram26@gmail.com>
2025-11-29 17:09:43 +0530

    new commit

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 
